0d8032acf7a649e49fc5ac283b57d4fc5827bbf2
Brandon Kelly <brandon@pixelandtonic.com>
2016-03-09 15:58:12 -0800

    Don't rely on Craft's Rich Text settings template

--- redactori/fieldtypes/RedactorIFieldType.php
+++ redactori/fieldtypes/RedactorIFieldType.php
@@ -55,7 +55,7 @@ class RedactorIFieldType extends BaseFieldType
 			'mediumtext' => Craft::t('MediumText (stores about 4GB)')
 		);
 
-		return craft()->templates->render('_components/fieldtypes/RichText/settings', array(
+		return craft()->templates->render('redactori/settings', array(
 			'settings' => $this->getSettings(),
 			'configOptions' => $configOptions,
 			'columns' => $columns,
